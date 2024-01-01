5450bf1eb1d28ac1ebb61828b6d662abd5a41a50
Prathamesh Zingade <101877850+ZingadePrathamesh@users.noreply.github.com>
2024-01-01 16:36:53 +0530

    Add files via upload

---
+++ event-manager-rest-service/src/test/java/com/example/eventmanagerrestservice/EventManagerRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventmanagerrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
