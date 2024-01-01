7f9549e2b24072aa4963eb22971ba2991426fe57
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-01-01 16:31:17 +0530

    making the rest service

---
+++ event-manager-rest-service/src/main/java/com/example/eventmanagerrestservice/EventManagerRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventmanagerrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerRestServiceApplication.class, args);
+	}
+
+}
