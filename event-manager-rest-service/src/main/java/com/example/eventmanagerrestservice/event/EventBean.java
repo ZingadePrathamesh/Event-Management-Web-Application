a141fd22f24fc550867d80a85cf694201a5eccfd
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2024-01-01 16:40:57 +0530

    g

--- event-manager-rest-service/src/main/java/com/example/eventmanagerrestservice/event/EventBean.java
+++ event-manager-rest-service/src/main/java/com/example/eventmanagerrestservice/event/EventBean.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.eventmanagerrestservice.event;
 
 import java.time.LocalDate;
@@ -50,56 +49,4 @@ public class EventBean {
 	
 	
 }
-=======
-package com.example.eventmanagerrestservice.event;
 
-import java.time.LocalDate;
-
-public class EventBean {
-	private Integer id;
-	private String name;
-	private String status;
-	private LocalDate targetDate;
-	public EventBean(Integer id, String name, String status, LocalDate targetDate) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.status = status;
-		this.targetDate = targetDate;
-	}
-	public EventBean() {
-		super();
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public LocalDate getTargetDate() {
-		return targetDate;
-	}
-	public void setTargetDate(LocalDate targetDate) {
-		this.targetDate = targetDate;
-	}
-	
-	@Override
-	public String toString() {
-		return "EventBean [id=" + id + ", name=" + name + ", status=" + status + ", targetDate=" + targetDate + "]";
-	}
-	
-	
-}
->>>>>>> 6325acc6e9513f1e73f67397ba4ee1bc0d15b039
